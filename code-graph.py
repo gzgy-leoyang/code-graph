6a13b6c6bbc4ac6103ea93e27f1d4c5a21ebc913
gzgy-leoyang <leoyang20102013@163.com>
2020-03-01 10:20:03 +0800

    遍历文件夹下所有C文件，按函数表示调用关系

--- code-graph.py
+++ code-graph.py
@@ -13,19 +13,17 @@ import string
 ##
 ##############
 class Function:
-    name=""
-    call_func_list = []
     def __init__(self,func_name):
         self.name = func_name
+        self.call_func_list=[]
 
 ##############
 ## 
 ##############
 class Src_file:
-    src_file_name = ""
-    func_list = []
     def __init__(self,file_name):
         self.src_file_name = file_name
+        self.func_list = []
     
 ###########################
 def get_func_name(ll ):
@@ -44,7 +42,7 @@ def get_void_func_name(ll ):
 
 #############################
 def parse_file(  src_file_name ):
-    # 匹配函数定义
+    # 函数头，类型1，花括号与函数名不同行
     rgl_def_func_name = r'''
     (((\s*)(static)(\s*)) | (\s*))  
     ((void)|(char)|(short)|(int)|(float)|(long)|(double)) # 识别函数返回值类型
@@ -54,11 +52,9 @@ def parse_file(  src_file_name ):
     ((\s*)?(const)?(\s*)?                                                             # 参数前是否有const
     ((void)|(char)|(short)|(int)|(float)|(long)|(double)) # 参数类型
     (\s*)(\*)?(\s*)?(restrict)?(\s*)?(\w+)(\s*)?(\,)?(\n)?(.*)?)*# 最后的*表示有多个参数
-    ((\s*)(\))(\n)?)                                                                         # 函数结束小括号
+    (((\s*)(\))(\s*)){1})                                                                         # 函数结束小括号
     '''
-    rgl_def_func_start = r'''
-    (({)(\n)?)# 函数开始"{"
-    '''
+
     rgl_def_func_end = r'''
     (})(\s*) # 函数结束"}"
     '''
@@ -71,57 +67,68 @@ def parse_file(  src_file_name ):
     rgl_call_void_func = r'''
     ((\s*)((?!if|while|switch)\w+)(\s*)(\())# "空白字符(>=0)-单词字符(>1)-空格字符(>=0)-( " (0次或1次)识别函数名
     '''
-    file_path  = sys.path[0] +'/'+ src_file_name
+    # file_path  = sys.path[0] +'/'+ src_file_name
+    file_path  = src_file_name
+
     scan_func_step = 0
-    def_func_obj = None
+    def_func = None
     with open( file_path ) as fd:
+        # 函数头
         def_func_patten_name = re.compile(rgl_def_func_name,re.X)
-        def_func_patten_start = re.compile(rgl_def_func_start,re.X)
+        # 调用函数
+        ## 类型1，有返回参数
         call_func_patten = re.compile(rgl_call_func,re.X)
-        call_void_func_patten = re.compile(rgl_call_void_func,re.X)
+        ## 类型2，无参数返回
+        call_void_func_patten = re.compile(rgl_call_void_func,re.X) 
+        # 函数尾
         def_func_patten_end = re.compile(rgl_def_func_end,re.X)
 
+        print ( "< %s >" %src_file_name )
         src_file = Src_file( src_file_name )
         for line in fd:
             if scan_func_step == 0 :
                 if def_func_patten_name.match( line ):
                     scan_func_step = 1
                     n = get_func_name( line )
-                    def_func_obj = Function( n )
-                    src_file.func_list.append( def_func_obj )
-                    print( "DEF: ",n,def_func_obj )
+                    def_func = Function( n )
+                    src_file.func_list.append( def_func )
             elif scan_func_step==1:
-                if def_func_patten_start.match( line ):
-                    scan_func_step = 2
-                    print( ">>")
-            elif scan_func_step==2:
                 if call_func_patten.match( line ):
                     n = get_func_name( line )
-                    if def_func_obj != None:
-                        f = Function( n )
-                        def_func_obj.call_func_list.append( f )
-                        print("+",def_func_obj,n,f)
+                    f = Function( n )
+                    def_func.call_func_list.append( f )
                 elif call_void_func_patten.match( line ):
                     n =  get_void_func_name( line )
-                    if def_func_obj != None:
-                        f = Function( n )
-                        def_func_obj.call_func_list.append( f )
-                        print("+",def_func_obj,n,f)
+                    f = Function( n )
+                    def_func.call_func_list.append( f )
                 elif def_func_patten_end.match( line ):
                     scan_func_step = 0
-                    def_func_obj = None
-                    print( "<<")
+                    def_func = None
+    
+    for f in src_file.func_list:
+        print("*",f.name)
+        for ff in f.call_func_list :
+            print("     |---",ff.name)
+
     return src_file
 
+
+def get_filelist(dir):
+    Filelist = []
+    for home, dirs, files in os.walk( dir ):
+        for filename in files:
+            if filename.endswith(".c") :
+                Filelist.append(os.path.join(home, filename))
+    return Filelist
 #################
 def main():
-    src_file_obj = parse_file( "can_service.c" )    
-    for f in src_file_obj.def_func_list:
-        print("*",f,f.name)
-        for ff in f.call_func_list :
-            print("**",ff.name)
+    file_list = get_filelist( sys.path[0]+"/src" )
+    for f in file_list:
+        src_file_obj = parse_file( f )
+        
     return 
 
+    # src_file_obj = parse_file( sys.path[0]+"/can_service.c" )
 #########
 if __name__ == "__main__":
     main()
